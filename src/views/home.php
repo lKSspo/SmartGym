cfead61e7ce6fb31ff43af0b2c228ebe03ee9ad6
Alessandro0003 <alessandroquinta3@gmail.com>
2024-05-30 21:05:33 -0300

    :fireworks: feat: adicionando logica para quando realizar o login aparecer a priemira letra do nome no icone da home page

--- src/views/home.php
+++ src/views/home.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+// print_r($_SESSION);
+
+// Caso não existir uma sessão com email ou password ele retornará para tela de login 
+if ((!isset($_SESSION['email']) === true) && (!isset($_SESSION['password']) === true)) {
+    unset($_SESSION['email']);
+    unset($_SESSION['password']);
+    header('Location: login.php');
+    exit();
+}
+
+// Pegando o email e nome da sessão
+$logado = $_SESSION['email'];
+$nome = $_SESSION['name'];
+
+// Pegando a primeira letra do nome
+$primeiraLetra = strtoupper(substr($nome, 0, 1));
+?>
+
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
@@ -10,7 +30,11 @@
     <link rel="stylesheet" href="../public/styles/home.css">
 </head>
 <body>
+    <div>
+        <div class="icone"><?php echo $primeiraLetra; ?></div>
+    </div>        
     <div class="header">
+        
         <div class="left-buttons">
             <button type="button" class="glow-on-hover">Home</button>
             <button type="button" class="glow-on-hover">Treinos</button>
