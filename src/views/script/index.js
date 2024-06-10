30622c7f8f078c6799e619b5b2cd3fcee8aadfe7
Alessandro0003 <alessandroquinta3@gmail.com>
2024-06-10 00:08:59 -0300

    🔥 feat: adicionando funcao para o modal de informacoes de usuario

--- src/views/script/index.js
+++ src/views/script/index.js
@@ -10,3 +10,18 @@ function togglePasswordVisibility() {
         iconVisible.name = 'eye-outline';
     }
 }
+
+
+document.querySelector('.icone').addEventListener('click', function() {
+    // Exibindo a caixa de informações
+    document.getElementById('infoBox').style.display = 'block';
+});
+
+// Adicionando evento de clique no documento para fechar o modal ao clicar fora dele
+document.addEventListener('click', function(event) {
+    var infoBox = document.getElementById('infoBox');
+    var icone = document.querySelector('.icone');
+    if (event.target !== infoBox && event.target !== icone) {
+        infoBox.style.display = 'none';
+    }
+});
